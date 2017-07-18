e2307abde83ea19cfb03dffadb834f8fc012349d
zf2169 <zf2169@columbia.edu>
2017-07-18 18:43:10 -0400

    new

---
+++ leetcode.R
@@ -0,0 +1,323 @@
+solution1 <- function(str) {
+  s<- strsplit(str, split = " ")
+  s<- unlist(s)
+  n<- length(s)
+  result<- s[n]
+  while(n>=1) {
+    n<- n-1
+    result<- cbind(result, s[n])
+  }
+  return(result)
+}
+
+solution1(str)
+
+solution2<- function(number) {
+  num<- strsplit(as.character(number), split = "")
+  num<- unlist(num)
+  for (i in 0:9) {
+    judge<- sum(num==as.character(i))
+    if(judge==1) {break}
+  }
+  return(i)
+}
+solution2(number)
+
+
+##########Two Sum##########
+# twosum<- function(nums, target) {
+#   n<-length(nums)
+#   judge<-0
+#   for(i in 1:(n-1)) {
+#     for(j in (i+1):n) {
+#       if(nums[i]+nums[j]==target) {
+#         judge<- 1
+#         break}
+#     }
+#     if(judge==1) {break}
+#   }
+#   return(c(i-1,j-1))
+# }
+# twosum(c(2,7,11,15), 9)
+
+twosum<- function(nums, target) {
+  n<-length(nums)
+  judge<-0
+  for(i in 1:(n-1)) {
+    for(j in (i+1):n) {
+      if(nums[i]+nums[j]==target) { return(c(i-1,j-1))}
+    }
+  }
+}
+twosum(c(2,7,11,15), 13)
+
+##########add two numbers#########
+addtwonumber<- function(a,b) {
+  n<- max(length(a), length(b))
+  anew<- rbind(a, rep(0, n - length(a)))
+  bnew<- rbind(b, rep(0, n - length(b)))
+  c<- rep(NA, n)
+  j<- 0
+  for(i in 1:n) {
+    x<- a[i]+b[i]+j
+    c[i]<- ifelse(x<10, x, x-10)
+    j<- ifelse(x<10, 0, 1)
+  }
+  return(c)
+}
+addtwonumber(c(8,5,3), c(5,6,4))
+
+###########longest substring##########
+longestsubatr<- function(s) {
+  n<- nchar(s)
+  ans_length<- 0
+  for(i in 1:n) {
+    for(j in i:n) {
+      subs<- substr(s, start = i, stop = j)
+      judge_rep<- max(table(strsplit(subs,"")))
+      judge_len<- length(table(strsplit(subs,"")))
+      if(judge_rep==1&judge_len>ans_length) {
+        ans<- subs
+        ans_length<- nchar(subs)
+      }
+    }
+  }
+  return(data.frame(answer=ans, ans_length ))
+  
+}
+longestsubatr("abcabcbb")
+longestsubatr("bbbbbb")
+longestsubatr("pwwkew")
+
+##########Median of Two Sorted Arrays#########
+sol_4<- function(nums1,nums2) {
+  num<- c(nums1, nums2)
+  return(median(num))
+}
+sol_4(c(1,3), c(2))
+sol_4(c(1,2), c(3,4))
+
+##########Longest Palindromic Substring##########
+sol_5<- function(input) {
+  n<- nchar(input)
+  out_len<- 0
+  for(i in 1:(n-1)) {
+    for(j in (i+1):n) {
+      subs<- substr(input, start = i, stop = j)
+      ss<- unlist(strsplit(subs, ""))
+      judge<- 1
+      nn<- nchar(subs)
+      for(i in 1:(floor(nn/2))) {
+        if(ss[i]!=ss[nn+1-i]) {
+          judge<- 0
+          break
+        }
+      }
+      if(judge==1&nn>out_len) {
+        output<- subs
+        out_len<- nchar(output)
+      }
+    }
+  }
+  return(output)
+}
+sol_5("babad")
+sol_5("babab")
+sol_5("cbb")
+
+########## ZigZag Conversion##########
+sol_6<- function(string, row_num) {
+  ss<- unlist(strsplit(string, ""))
+  count<- nchar(string)
+  ans<- rep(NULL, row_num)
+  c<- 1
+  while(c<= count) {
+    part<- matrix("", nrow = row_num, ncol = row_num-1)
+    for(i in 1:row_num) {
+      part[i,1]<- ss[c]
+      c<- c+1
+    }
+    for(j in 2:(row_num-1)) {
+      part[(row_num+1-j),j]<-ss[c]
+      c<- c+1
+    }
+    ans<- cbind(ans, part)
+  }
+  ans<- ifelse(is.na(ans), "", ans)
+  ans<- ans[, colSums(ans=="")!=row_num]
+  result<- paste(t(ans), collapse = "")
+  return(list(ans, result))
+}
+sol_6("PAYPALISHIRING",3)
+sol_6("PAYPALISHIRING",5)
+
+##########Reverse Integer##########
+sol_7<- function(x) {
+  char<- unlist(strsplit(as.character(abs(x)), ""))
+  digit<- length(char)
+  reverse<- NULL
+  for(i in 1:digit) {
+    reverse<- paste(reverse, char[digit+1-i], sep = "")
+  }
+  reverse<- as.double(reverse)
+  reverse<- ifelse(x>0, reverse, -reverse)
+  imax<- .Machine$integer.max
+  if(reverse>imax || reverse<(-imax)) {return(0)}
+  else {return(reverse)}
+}
+sol_7(123)
+sol_7(-12323423)
+sol_7(1000000002)
+
+##########String to Integer (atoi)##########
+sol_8<- function(string) {
+  expr<- "[0-9]{1}"
+  ans<- unlist(regmatches(string, gregexpr(pattern = expr, string)))
+  return(paste(ans, collapse = ""))
+}
+sol_8("2342jjhj00798---=-=#@%^$939")
+
+#########Palindrome Number##########
+sol_9<- function(number) {
+  num<- unlist(strsplit(as.character(number), split = ""))
+  n<- length(num)
+  judge<- 1
+  for(i in 1:floor(n/2)) {
+    if(num[i]!=num[n+1-i]) { judge<- 0 }
+  }
+  return(ifelse(judge==1, "yes", "no"))
+}
+sol_9(123321)
+sol_9(1234321)
+sol_9(12345789)
+sol_9(123456)
+
+##########Regular Expression Matching##########
+isMatch<- function(string1, string2) {
+  if(nchar(string2) < nchar(string1)) {return(as.logical(0))}
+  n<- nchar(string2)
+  ans<- 0
+  for(i in 1:n) {
+    for(j in 1:n) {
+      sub2<- substr(string2, start = i, stop = j)
+      matchjudge<- unlist(regmatches(string1, 
+                                gregexpr(pattern = sub2, string1)))
+      judge<- sum(matchjudge==string1)
+      if(judge!=0) {ans<- 1}
+    }
+  }
+  return(as.logical(ans))
+}
+isMatch("aa","a")
+isMatch("aa","aa")
+isMatch("aab", "c*a*b")
+isMatch("ab", ".*")
+
+##########Container With Most Water##########
+sol_11<- function(vec) {
+  n<- length(vec)
+  ans<- 0
+  for(i in 1:(n-1)) {
+    for(j in (i+1):n) {
+      height<- min(vec[1], vec[j])
+      width<- j-i
+      volume<- height*width
+      if(volume>ans) {
+        ans<- volume
+        index<- cbind(x=c(i,j), y=c(vec[i], vec[j]))
+      }
+    }
+  }
+  return(list(ans, index))
+}
+sol_11(c(3,5,2,1,1))
+
+##########Integer to Roman##########
+as.roman(1233)
+
+##########Roman to Integer##########
+sol_13<- function(roman) {
+  return(as.numeric(as.roman(roman)))
+}
+sol_13("MCCXXXIII")
+
+##########Longest Common Prefix##########
+sol_14<- function(strarray) {
+  n<- length(strarray)
+  l<- min(sapply(strarray, nchar))
+  for(i in 1:l) {
+    subarray<- sapply(strarray, substr, start=1, stop=i)
+    test<- subarray[1]
+    if(sum(subarray==test)!=n) {
+      test<- substr(strarray[1], start = 1, stop = (i-1))
+      break}
+  }
+  return(test)
+}
+
+strarray<- c("abcdasf", "abcdwet", "abcdyuiiyu","abcdjlhjkh")
+strarray<- rep("abcd", 5)
+sol_14(strarray)
+
+##########3Sum##########
+threesum<- function(numarray) {
+  n<- length(numarray)
+  result<- NULL
+  for(i in 1:(n-2)) {
+    for(j in (i+1):(n-1)) {
+      for(k in (j+1):n) {
+        s<- sum(numarray[i]+numarray[j]+numarray[k])
+        if(s==0) {
+          ans<- c(numarray[i], numarray[j], numarray[k])
+          result<- rbind(result, ans)
+        }
+      }
+    }
+  }
+  result<- result[!duplicated(t(apply(result, 1, sort))),]
+  return(result)
+}
+s<- c(-1, 0, 1, 2, -1, -4)
+threesum(s)
+
+a <- c(rep("A", 3), rep("B", 3), rep("C",2))
+b <- c(1,1,2,4,1,1,2,2)
+df <-data.frame(a,b)
+
+duplicated(df)
+
+
+
+
+
+
+##########3Sum Closest##########
+sol_16<- function(array, target) {
+  n<- length(array)
+  ans_diff<- Inf
+  for(i in 1:(n-2)) {
+    for(j in (i+1):(n-1)) {
+      for(k in (j+1):n) {
+        s<- sum(array[i], array[j], array[k])
+        diff<- abs(s - target)
+        if(diff==0) {next}
+        if(diff<=ans_diff) {
+          ans_diff<- diff
+          ans_sum<- s
+          ans_index<- c(i,j,k)
+          }
+      }
+    }
+  }
+  return(list(number=array[ans_index],sum=ans_sum))
+}
+
+array<- c(-1,2,1,-4)
+target<- 1
+sol_16(array, target)
+
+
+
+
+
+
